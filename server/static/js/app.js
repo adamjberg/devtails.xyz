bea72463e6f0f443d5711e6572ed21635dfca299
Adam Berg <adam@xyzdigital.com>
2021-12-22 15:58:24 -0800

    Add start to server to simplify adding images

---
+++ server/static/js/app.js
@@ -0,0 +1,83 @@
+async function run() {
+  const pathname = document.location.pathname;
+
+  switch (pathname) {
+    case "/":
+      Home();
+      break;
+    default:
+      Post();
+      break;
+  }
+}
+
+async function Home() {
+  const res = await fetch("/api/posts");
+  const jsonData = await res.json();
+  const { data: posts } = jsonData;
+
+  const root = document.getElementById("root");
+  for (const post of posts) {
+    const div = document.createElement("div");
+    const linkEl = document.createElement("a");
+    linkEl.setAttribute("href", `/posts/${post}`);
+    linkEl.innerText = post;
+    div.appendChild(linkEl);
+    root.appendChild(div);
+  }
+}
+
+async function Post() {
+  const pathname = document.location.pathname;
+  const apiPath = `/api${pathname}`;
+  const res = await fetch(apiPath);
+  const jsonData = await res.json();
+  const { data: post } = jsonData;
+
+  const root = document.getElementById("root");
+
+  const textarea = document.createElement("textarea");
+  textarea.style.display = "block";
+  textarea.style.width = "100%";
+  textarea.style.height = "90%";
+  textarea.style.whiteSpace = "pre-wrap";
+  textarea.innerHTML = post;
+
+
+  textarea.addEventListener("drop", async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.dataTransfer.files.length) {
+      const file = e.dataTransfer.files[0];
+      const fd = new FormData();
+      fd.append('image', file);
+
+      const res = await fetch('/api/upload', {
+          method: 'POST',
+          body: fd
+      });
+      const { data: filename } = await res.json();
+    }
+  });
+
+  root.appendChild(textarea);
+
+  const saveBtn = document.createElement("button");
+  saveBtn.innerText = "Save";
+  saveBtn.addEventListener("click", () => {
+    fetch(apiPath, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        body: simplemde.value(),
+      }),
+    });
+  });
+
+  root.appendChild(saveBtn);
+}
+
+run();
